856514c120d1ecf955c0d3cb8be7c869f8f675f9
Homie-Khanh <156377690+Homie-Khanh@users.noreply.github.com>
2024-02-14 19:07:01 -0500

    a3 labs - stop at 2.2.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Labs from './Labs';
+import HelloWorld from './Labs/a3/HelloWorld';
+import Kanbas from './Kanbas';
 
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <Labs/>
+      <Kanbas/>
+      <HelloWorld/>
+    </div>
   );
 }
 
