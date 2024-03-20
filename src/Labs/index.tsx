4f5715fad0dd988d9e3b4100fa5607df65bb9d9d
Homie-Khanh <156377690+Homie-Khanh@users.noreply.github.com>
2024-03-20 11:16:48 -0400

    fixing routes and file

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,6 +1,6 @@
 import Assignment3 from "./a3";
 import Nav from "../Nav";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Assignment4 from "./a4";
 import store from "./store";
 import { Provider } from "react-redux";
@@ -12,6 +12,7 @@ function Labs() {
          <h1>Labs</h1>
          <Nav/> 
          <Routes>
+            <Route path="/" element={<Navigate to="a3"/>}/>
             <Route path="/a3/*" element={<Assignment3 />} />
             <Route path="/a4/*" element={<Assignment4 />} />
          </Routes>
